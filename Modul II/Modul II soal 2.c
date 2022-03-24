1248064c6574477b826fc5f0f123de2d49e8c160
2105551126_I Nyoman Yodya Mahesa Sastra <77004260+YodyaMahesa@users.noreply.github.com>
2022-03-24 21:59:26 +0800

    Update Modul II soal 2.c

--- Modul II/Modul II soal 2.c
+++ Modul II/Modul II soal 2.c
@@ -2,108 +2,189 @@
 #include <stdio.h>
 #include <string.h>
 
-int hitungKehadiran(int kehadiran){
-	//max 5%
-	return kehadiran * 100/15 * 5/100;
+void header(){
+	//Header Program
+	printf("\n");
+	printf("\t\t\t\t\t*---------------------------------*\n");
+	printf("\t\t\t\t\t|   Program Penentu Nilai Akhir   |\n");
+	printf("\t\t\t\t\t*---------------------------------*\n");
+	printf("\n");
 }
 
-int hitungNilai3Tugas(int tugas1, int tugas2, int tugas3){
-	//max 20%
-	return (tugas1+tugas2+tugas3)/3 * 20/100;
+void masukkanJikaAngka(int* pilihan,int* cekerror,int skala,char* teks){	
+	char sementara[255];
+	char tampungNewline;
+	while(1){
+		printf(teks);
+		fgets(sementara,sizeof(sementara),stdin);
+		if(sscanf(sementara,"%d %s",pilihan,&tampungNewline)==1){
+			if(*pilihan<0||*pilihan>skala){
+				printf("\t\t\t\t\tSALAH! - Input harus 0 sampai %d!\n\n",skala);
+				*cekerror=1;
+				break;
+			}	
+			*cekerror=0;
+			system("cls");
+			header();
+			break;
+		}	
+		else{
+		printf("\t\t\t\t\tSALAH! - Input harus berupa angka!\n\n");
+		fflush(stdin);
+		}
+	}
 }
 
-int hitungNilaiQuiz(int nilaiQuiz){
-	//max 15%
-	return nilaiQuiz * 15/100;
+int ulangProgram(){
+	char sementara[255];
+	char tampungNewline;
+	int ulangi;
+	int hasil;
+	
+	printf("\n");
+	printf("\t\t\t\t\t[1]= Ya\n");
+	printf("\t\t\t\t\t[0]= Tidak\n");
+	printf("\t\t\t\t\tIngin mengulang program? : ");
+	
+	fgets(sementara,sizeof(sementara),stdin);
+	
+	if(sscanf(sementara,"%d %s",&ulangi,&tampungNewline)==1){
+		if(ulangi==1){
+			system("cls");
+			fflush(stdin);
+			return main();		
+		}
+		else if(ulangi==0){
+			system("cls");
+			fflush(stdin);
+			header();
+			return tutupProgram();
+		}
+	}
+	system("cls");
+	fflush(stdin);
+	return tidakValid();			
 }
 
-int hitungNilaiUTS(int nilaiUTS){
-	//max 30%
-	return nilaiUTS * 30/100;
+int tidakValid(){
+	header();
+	printf("\t\t\t\t\tSALAH! - Input tidak valid\n");
+	return ulangProgram();	
 }
 
-int hitungNilaiUAS(int nilaiUAS){
-	//max 30%
-	return nilaiUAS * 30/100;
-}
-
-int hitungNilaiAkhir(int akhirKehadiran,int akhirNilai3Tugas,int akhirNilaiQuiz,int akhirNilaiUTS,int akhirNilaiUAS){
+int hitungNilaiAkhir(int Kehadiran,int Nilai3Tugas,int NilaiQuiz,int NilaiUTS,int NilaiUAS){
 	//hitung semua max 100
-	return akhirKehadiran+akhirNilai3Tugas+akhirNilaiQuiz+akhirNilaiUTS+akhirNilaiUAS;
+	return Kehadiran+Nilai3Tugas+NilaiQuiz+NilaiUTS+NilaiUAS;
 }
 
 void hitungPredikatNilai(int nilaiAkhir,char* predikatNilai){
 	if(nilaiAkhir>=0 && nilaiAkhir<45){
-		strcpy(predikatNilai,"E+");
+		strcpy(predikatNilai,"E");
+	}
+	else if(nilaiAkhir>=45 && nilaiAkhir<50){
+		strcpy(predikatNilai,"D");
+	}
+	else if(nilaiAkhir>=50 && nilaiAkhir<55){
+		strcpy(predikatNilai,"D+");
+	}
+	else if(nilaiAkhir>=55 && nilaiAkhir<60){
+		strcpy(predikatNilai,"C");
+	}
+	else if(nilaiAkhir>=60 && nilaiAkhir<65){
+		strcpy(predikatNilai,"C+");
+	}
+	else if(nilaiAkhir>=65 && nilaiAkhir<75){
+		strcpy(predikatNilai,"B");
+	}
+	else if(nilaiAkhir>=75 && nilaiAkhir<80){
+		strcpy(predikatNilai,"B+");
+	}
+	else if(nilaiAkhir>=80 && nilaiAkhir<=100){
+		strcpy(predikatNilai,"A");
 	}
 }
 
+int tutupProgram(){
+	//Tutup Program
+	printf("\t\t\t\t\t         Selamat Tinggal          \n");
+	Sleep(1000);
+	return 1;
+}
+
 int main(){
-	//Deklarasi Variabel Inputan User
+	int cekerror=0;
+	char predikatNilai[2];
+	
+	//Deklarasi Variabel
 	int kehadiran;
 	int nilaiTugas1;
 	int nilaiTugas2;
 	int nilaiTugas3;
+	int nilai3Tugas;
 	int nilaiQuiz;
 	int nilaiUTS;
 	int nilaiUAS;
+	int nilaiAkhir;	
 	
-	//Deklarasi Variabel Telah Terhitung
-	int akhirKehadiran;
-	int akhirNilai3Tugas;
-	int akhirNilaiQuiz;
-	int akhirNilaiUTS;
-	int akhirNilaiUAS;
-	int nilaiAkhir;
-	char predikatNilai[1];
-	
-	//Header Program
-	printf("Program Penentu Nilai Akhir (NA)\n");
-	printf("--------------------------------\n");
+	header();
 	
 	//Input Kehadiran
-	printf("Kehadiran (Max15): ");
-	scanf("%d",&kehadiran);
+	do{
+		masukkanJikaAngka(&kehadiran,&cekerror,15,"\t\t\t\t\tMasukkan Kehadiran (Max15): ");
+	}while(cekerror==1);
+
+	//Proses Kehadiran MAX 5%
+	kehadiran = kehadiran * 100/15 * 5/100;
 	
 	//Input Nilai Tugas 1
-	printf("Nilai Tugas 1    : ");
-	scanf("%d",&nilaiTugas1);
-	
+	do{
+		masukkanJikaAngka(&nilaiTugas1,&cekerror,100,"\t\t\t\t\tMasukkan Nilai Tugas 1    : ");
+	}while(cekerror==1);
+
 	//Input Nilai Tugas 2
-	printf("Nilai Tugas 2    : ");
-	scanf("%d",&nilaiTugas2);
+	do{
+		masukkanJikaAngka(&nilaiTugas2,&cekerror,100,"\t\t\t\t\tMasukkan Nilai Tugas 2    : ");
+	}while(cekerror==1);
 	
 	//Input Nilai Tugas 3
-	printf("Nilai Tugas 3    : ");
-	scanf("%d",&nilaiTugas3);
+	do{
+		masukkanJikaAngka(&nilaiTugas3,&cekerror,100,"\t\t\t\t\tMasukkan Nilai Tugas 3    : ");
+	}while(cekerror==1);
+	
+	//Proses Ke3 Nilai Tugas MAX 20%
+	nilai3Tugas = (nilaiTugas1+nilaiTugas2+nilaiTugas3)/3 * 20/100;
 	
 	//Input Nilai Quiz
-	printf("Nilai Quiz       : ");
-	scanf("%d",&nilaiQuiz);
+	do{
+		masukkanJikaAngka(&nilaiQuiz,&cekerror,100,"\t\t\t\t\tMasukkan Nilai Quiz       : ");
+	}while(cekerror==1);
+	
+	//Proses Nilai Quiz MAX 15%
+	nilaiQuiz = nilaiQuiz * 15/100;
 	
 	//Input Nilai UTS
-	printf("Nilai UTS        : ");
-	scanf("%d",&nilaiUTS);
+	do{
+		masukkanJikaAngka(&nilaiUTS,&cekerror,100,"\t\t\t\t\tMasukkan Nilai UTS        : ");
+	}while(cekerror==1);
+	
+	//Proses Nilai UTS MAX 30%
+	nilaiUTS = nilaiUTS * 30/100;
 	
 	//Input Nilai UAS
-	printf("Nilai UAS        : ");
-	scanf("%d",&nilaiUAS);
+	do{
+		masukkanJikaAngka(&nilaiUAS,&cekerror,100,"\t\t\t\t\tMasukkan Nilai UAS        : ");
+	}while(cekerror==1);
 	
-	//Proses Nilai
-	akhirKehadiran   = hitungKehadiran(kehadiran);
-	akhirNilai3Tugas = hitungNilai3Tugas(nilaiTugas1,nilaiTugas2,nilaiTugas3);
-	akhirNilaiQuiz   = hitungNilaiQuiz(nilaiQuiz); 
-	akhirNilaiUTS    = hitungNilaiUTS(nilaiUTS);
-	akhirNilaiUAS    = hitungNilaiUAS(nilaiUAS);
-	nilaiAkhir		 = hitungNilaiAkhir(akhirKehadiran,akhirNilai3Tugas,akhirNilaiQuiz,akhirNilaiUTS,akhirNilaiUAS);
-	hitungPredikatNilai(nilaiAkhir,&predikatNilai[0]);
+	//Proses Nilai UAS MAX 30%
+	nilaiUAS = nilaiUAS * 30/100;
 	
-	printf("--------------------------------\n");
-	printf("Nilai Akhir      : %d\n",nilaiAkhir);
-	printf("Predikat Nilai   : %s\n",predikatNilai);
+	//Proses Nilai Akhir dan Predikat
+	nilaiAkhir		 = hitungNilaiAkhir(kehadiran,nilai3Tugas,nilaiQuiz,nilaiUTS,nilaiUAS);
+	hitungPredikatNilai(nilaiAkhir,predikatNilai);
 	
-	Sleep(1000);
-	
-	//Akhir Program
-	return 0;
+	printf("\t\t\t\t\t----------------------------------\n");
+	printf("\t\t\t\t\tNilai Akhir      : %d\n",nilaiAkhir);
+	printf("\t\t\t\t\tPredikat Nilai   : %s\n",predikatNilai);
+	printf("\t\t\t\t\t----------------------------------\n");
+	return ulangProgram();	
 }
